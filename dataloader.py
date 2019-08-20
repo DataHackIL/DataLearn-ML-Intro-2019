db956da1e45265c7c8a6b11468cf94610c180ce9
Shay Palachy <shaypal5@gmail.com>
2019-08-20 15:38:16 +0300

    dataloader should be done

--- dataloader.py
+++ dataloader.py
@@ -4,14 +4,17 @@ import pip
 import pandas as pd
 
 try:
-    import google.colab
+    import google.colab  # noqa: F401
+
     IN_COLAB = True
 except ImportError:
     IN_COLAB = False
 
+
 def in_notebook():
     try:
         from IPython import get_ipython
+
         if 'IPKernelApp' not in get_ipython().config:  # pragma: no cover
             return False
     except ImportError:
@@ -26,21 +29,39 @@ def pipinstall(package):
         pip._internal.main(['install', package])
 
 
-GDRIVE_AUTHENICATED = False
+GDRIVE = None
+
 
 def gdrive_authenticate():
-    global GDRIVE_AUTHENICATED
-    if GDRIVE_AUTHENICATED:
+    global GDRIVE
+    if GDRIVE is not None:
         return
+    print('Installing PyDrive...')
     pipinstall('PyDrive')
     # !pip install -U -q PyDrive
     from pydrive.auth import GoogleAuth
     from pydrive.drive import GoogleDrive
     from google.colab import auth
     from oauth2client.client import GoogleCredentials
+
     # Authenticate and create the PyDrive client.GDRIVE_AUTHENICATED# This only
     # needs to be done once per notebook.
+    print('Authenticating with Google Drive...')
     auth.authenticate_user()
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
-    drive = GoogleDrive(gauth)
+    GDRIVE = GoogleDrive(gauth)
+
+
+def get_accounts():
+    if IN_COLAB:
+        gdrive_authenticate()
+        # you can see it with "get sherable link"
+        print("Downloading accounts.csv from Google Drive...")
+        accounts_file_id = '1SFFGL_FIq3-l6CP9MTe9ueuLRMz_tvrw'
+        downloaded = GDRIVE.CreateFile({'id': accounts_file_id})
+        downloaded.GetContentFile('accounts.csv')
+        print("Done.")
+        return pd.read_csv('accounts.csv')
+    else:
+        return pd.read_csv('data/accounts.csv')
